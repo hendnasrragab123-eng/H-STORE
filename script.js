4de991b3e0e4cbc7c04df7c8b9dbec42544e7856
hendnasrragab123-eng <hendnasrragab123@gmail.com>
2026-07-27 22:49:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -413,3 +413,19 @@ function sendOrderToWhatsApp(event) {
   window.open(whatsappURL, '_blank');
   closeCheckout();
 }
+// ⬆️ إظهار وإخفاء زر الرجوع للأعلى
+window.onscroll = function() {
+  let btn = document.getElementById("scrollTopBtn");
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
