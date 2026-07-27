ea032712cdbf91378cb48923b1807e39064e59d7
hendnasrragab123-eng <hendnasrragab123@gmail.com>
2026-07-28 00:24:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -520,3 +520,19 @@ function scrollToTop() {
     behavior: 'smooth'
   });
 }
+// ⬆️ إظهار وإخفاء زر الرجوع للأعلى
+window.onscroll = function() {
+    let btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+  
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
